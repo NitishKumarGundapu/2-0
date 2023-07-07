aaa59c6d73a737a149332e87455b4c80233fa68a
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2023-07-07 11:51:58 +0530

    commited on 07/07 - uploaded 1-summer folder files

---
+++ python_coding/a.py
@@ -0,0 +1,2 @@
+from math import *
+print(log2(964176192  + 43261596))
---
+++ thesis/CL_in_rnn/main.py
@@ -0,0 +1,12 @@
+
+
+#Example : 
+
+V = ["Human","King","Queen","Woman"]
+
+#One hot represenation of the words are:
+
+Human = [1,0,0,0]
+King = [0,1,0,0]
+Queen = [0,0,1,0]
+Woman = [0,0,0,1]
